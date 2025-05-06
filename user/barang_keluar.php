98cd1ee94d942296eb25c473d0f3490bab5fea74
ayunisay <ayunisa1412@gmail.com>
2025-05-06 23:07:45 +0700

    up

--- user/barang_keluar.php
+++ user/barang_keluar.php
@@ -251,8 +251,7 @@ if (isset($_GET['edit'])) {
                 </div>
                 <div class="mt-4">
                     <label for="keterangan" class="block text-gray-600 font-medium mb-2">Keterangan Barang Keluar</label>
-                    <textarea id="keterangan" name="keterangan" rows="4" class="w-full px-1 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 text-left" placeholder="Masukkan keterangan" value="" required>  <?php echo $edit_data ? htmlspecialchars($edit_data['keterangan']) : ''; ?>
-                    </textarea>
+                    <textarea id="keterangan" name="keterangan" rows="4" class="w-full px-1 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 text-left" placeholder="Masukkan keterangan" required><?php echo $edit_data ? htmlspecialchars($edit_data['keterangan']) : ''; ?></textarea>
                 </div>
                 <button type="submit" name="<?php echo $edit_data ? 'update' : 'insert'; ?>" class="mt-4 bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition duration-300">
                     <?php echo $edit_data ? "Edit Barang Keluar" : "Tambah Barang Keluar"; ?>
